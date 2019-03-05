d1182a847320e1295ffc0bfec5075f77b29b2918
Vignesh Narasimhulu <vigindian@gmail.com>
2019-03-06 05:37:39 +0800

    logic changes

--- .travis.quality.sh
+++ .travis.quality.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-touch /tmp/test
+
+#Check formatting
 if [ -n "$(gofmt -l .)" ]; then
     echo "Go code is not formatted:"
     gofmt -d .
     exit 1
 fi
+
+#Check Security Issues
+CMD="gosec -quiet -exclude=G104 main.go"
+if [ -n "`$CMD`" ]; then
+   echo "Security Issues"
+   exit 2
+fi
+
